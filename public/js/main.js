edd64b8c32ece754cdac658dd80b3629969e62cf
Travis Spangle <travis.spangle@k2sports.com>
2016-08-13 15:27:19 -0700

    adding jquery and ajax call for request

--- public/js/main.js
+++ public/js/main.js
@@ -1,1 +1,9 @@
-
+$("#submit_emails").on("click", function(){ 
+  $.post( "uniq", { emails: $("#emails").val() })
+  .done(function( data ) {
+    $("#emails").val(data);
+  })
+  .fail(function() {
+    console.log("error processing request");
+  });
+});
