ea26a5a2a1dffeedaddf851030effbfbf90674a8
Travis Spangle <travis.spangle@k2sports.com>
2016-09-11 17:10:53 -0700

    adding js testing

---
+++ app/assets/email_generator.js
@@ -0,0 +1,8 @@
+function EmailGenerator() {
+}
+EmailGenerator.prototype.create_emails = function(amount) {
+};
+EmailGenerator.prototype.mix = function() {
+};
+EmailGenerator.prototype.duplicate_emails = function(percentage) {
+};
---
+++ test/spec/EmailGeneratorSpec.js
@@ -0,0 +1,19 @@
+describe("EmailGenerator", function() {
+  var email_generator;
+
+  beforeEach(function() {
+    email_generator = new EmailGenerator();
+  });
+
+  it("should generate test_1@email.com as it's first email", function(){
+
+  });
+
+  it("should duplicate email addresses", function(){
+
+  });
+
+  it("should mix email addresses", function(){
+
+  });
+});
